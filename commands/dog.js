4638c84e063f5adf7fa5113616b5da1a74417e73
pepijn <68758035+ItsMePepijn@users.noreply.github.com>
2021-08-09 15:17:46 +0200

    added dog command using the dog api

---
+++ commands/dog.js
@@ -0,0 +1,23 @@
+const { default: axios } = require("axios");
+const Discord = require('discord.js');
+
+module.exports = {
+	name: 'dog',
+	description: 'Sends a random dog',
+	execute(message) {
+        const embed = new Discord.MessageEmbed()
+		axios
+		.get('https://api.thedogapi.com/v1/images/search')
+		.then((res) => {
+            embed.setTitle("Your random dog:")
+            embed.setColor("#ECBCD7")
+            embed.setTimestamp()
+			embed.setImage(res.data[0].url)
+			message.channel.send(embed);
+			console.log('Embed sent')
+		})
+		.catch((err) => {
+			console.log('ERR:', err)
+		})
+	},
+};
