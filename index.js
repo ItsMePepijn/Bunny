f7cdad7b7f540fef497db61e79f990e13fb89f1d
pepijn <68758035+ItsMePepijn@users.noreply.github.com>
2021-11-06 19:34:19 +0100

    changed status

--- index.js
+++ index.js
@@ -157,7 +157,7 @@ client.on('interactionCreate', interaction => {
 });
 
 function status(){
-    client.user.setPresence({ activities: [{ name: 'my master', type: 'LISTENING' }], status: 'dnd' });
+    client.user.setPresence({ activities: [{ name: 'over the server', type: 'WATCHING' }], status: 'dnd' });
 }
 
 client.login(token);
