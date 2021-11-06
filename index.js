0d882ca59f574265f99aa001777f64af2c8b27de
pepijn <68758035+ItsMePepijn@users.noreply.github.com>
2021-11-06 14:49:00 +0100

    changed verify method into button

--- index.js
+++ index.js
@@ -11,6 +11,7 @@ const client = new Discord.Client({ intents: [
     DIF.GUILD_BANS,
     DIF.GUILD_MEMBERS,
     DIF.DIRECT_MESSAGES,
+    DIF.GUILD_INTEGRATIONS
 ], 
 partials: ['MESSAGE', 'CHANNEL', 'REACTION'] });
 
@@ -57,14 +58,8 @@ const {token} = require(__dirname + '/config.json');
 client.on('messageCreate', message => {
     const args = message.content.slice(pfx.length).trim().split(' ');
     if(message.channel.id == '899636638681006080'){
-        if(message.author.id == '872841043354198047') return
+        if(message.member.permissions.has('ADMINISTRATOR')) return
         if(args[0] != 'suggest') return message.delete();
-    }
-    if(message.channel.id == '898537277599404034'){
-        if(message.author.id == '872841043354198047') return
-        if(message.content != `${pfx}verify`){
-            return message.delete();
-        }
     }
     if (!message.content.startsWith(pfx) || message.author.bot || !message.guild) return;
     const command = args.shift().toLowerCase();
@@ -124,6 +119,38 @@ client.on('guildMemberAdd', member => {
         welcomechannel.send('<a:letterw:898615332418256947><a:lettere:898615332929929246><a:letterl:898615333038985226><a:letterc:898615332774744095><a:lettero:898615332950908928><a:letterm:898615332825104405><a:lettere:898615332929929246><a:exclamationmark:898615332724432957>')
         welcomechannel.send(`<a:letterh:898615637176356895><a:letteri:898615637423841290> ・ Nice to meet you ${member}!\n> ୨・Please first read the rules in <#863742019372711996>\n> ୨・Get your roles in <#863750202383794186>\n> ୨・Please verify in <#898537277599404034>!\n◇─◇──◇──────◇──◇─◇`)
 
+    }
+});
+
+client.on('interactionCreate', interaction => {
+    if(interaction.isButton){
+        if(interaction.customId == 'verify'){
+            const memberole = interaction.guild.roles.cache.get('898536580468334602');
+            const verifylogs = interaction.guild.channels.cache.get('898538121908613121');
+            const chat = interaction.guild.channels.cache.get('864532474398507048');
+            const embed = new Discord.MessageEmbed();
+            const embed2 = new Discord.MessageEmbed();
+            if(!interaction.member.roles.cache.has('898536580468334602')){
+                interaction.deferUpdate();
+                interaction.member.roles.add(memberole);
+                embed.setTitle(`${interaction.user.tag} has veen verified!`);
+                embed.setDescription(`<t:${Math.round(Date.now() / 1000)}:R>`);
+                embed.setFooter(`${interaction.member.id}`, `${interaction.member.displayAvatarURL({format: 'png', dynamic: true })}`);
+                embed.setColor("#ECBCD7");
+                embed.setTimestamp();
+
+                embed2.setTitle('A new member appeared!');
+                embed2.setDescription(`Give ${interaction.member} a warm welcome!`);
+                embed2.setFooter(`${interaction.member.id}`, `${interaction.member.displayAvatarURL({format: 'png', dynamic: true})}`);
+                embed2.setColor("#ECBCD7");
+                embed2.setTimestamp();
+
+                verifylogs.send({embeds: [embed] });
+                chat.send({embeds: [embed2] });
+                console.log('Embed sent');
+                console.log(`${interaction.user.tag} has veen verified!`)
+            }
+        }
     }
 });
 
