aefeebecb937a10b02df77e29f7e1d6dbee4055e
pepijn <68758035+ItsMePepijn@users.noreply.github.com>
2022-03-08 15:52:50 +0100

    ew hardcode

--- events/ready.js
+++ events/ready.js
@@ -10,7 +10,7 @@ module.exports = {
         console.log(chalk.greenBright(client.user.tag + ' Has logged in!'));
         client.user.setPresence({ activities: [{ name: 'all the cuties', type: 'WATCHING' }], status: 'dnd' });
 
-        const logs = client.channels.cache.find(channel => channel.id === db.get('channels.logs.start'));
+        const logs = client.channels.cache.get(db.get('channels.logs.start'));
         embed.setTitle('Bot started!')
         embed.setColor('#ECBCD7')
         embed.setTimestamp()
